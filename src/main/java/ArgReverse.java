126d4c80239ecf40d436a4ad6ac473410bccb940
Artyom Naidich <artemnaidichg@gmail.com>
2019-10-27 23:11:53 +0300

    Second task

---
+++ src/main/java/ArgReverse.java
@@ -0,0 +1,16 @@
+public class ArgReverse {
+    public static void main(String[] args) {
+
+
+        if (args.length!=0){
+            for (int i = args.length; i > 0; i--){
+                System.out.print(args[i-1] + " ");
+            }
+        } else {
+            System.out.println("Enter args, please");
+        }
+        System.out.println();
+
+
+    }
+}
